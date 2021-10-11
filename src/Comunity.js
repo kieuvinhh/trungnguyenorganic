696c4fba7bcbc2f9c9b970d487158b77c1b9a886
Brian Nguyen <brian.nguyen2902@gmail.com>
2021-10-11 16:27:57 +0700

    addComunity

---
+++ src/Comunity.js
@@ -0,0 +1,13 @@
+import react from "react";
+import "./Comunity.css";
+function Comunity() {
+  return (
+    <div>
+      <div className="Video1"> video 1</div>
+      <div className="video2"> video 1</div>
+      <div className="Video3"> video 1</div>
+      <div className="Video3"> video 1</div>
+    </div>
+  );
+}
+export default Comunity;
--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,27 @@
 import "./Header.css";
-// eslint-disable-next-line 
+// eslint-disable-next-line
 import react from "react";
-function Header(){
-    return(
-        <div className='header'>
-            <div className="navigation">
-                <div className='header__option'>
-                    <span className="Lineone">Trang chủ</span>
-                </div>
-                <div className='header__option'>
-                    <span className="Lineone">Giới thiệu</span>
-                </div>
-                <div className='header__option'>
-                    <span className="Lineone">Sản phẩm</span>
-                </div>
-                <div className='header__option'>
-                    <span className="Lineone">Tin tức sức khỏe</span>
-                </div>
-                <div className='header__option'>
-                    <span className="Lineone">Liên hệ</span>
-                </div>
-            </div>
+function Header() {
+  return (
+    <div className="header">
+      <div className="navigation">
+        <div className="header__option">
+          <span className="Lineone">Trang chủ</span>
         </div>
-    );
+        <div className="header__option">
+          <span className="Lineone">Giới thiệu</span>
+        </div>
+        <div className="header__option">
+          <span className="Lineone">Sản phẩm</span>
+        </div>
+        <div className="header__option">
+          <span className="Lineone">Tin tức sức khỏe</span>
+        </div>
+        <div className="header__option">
+          <span className="Lineone">Liên hệ</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Header;
