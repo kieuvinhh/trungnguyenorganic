21b1c8fcff6a53a818551836ef5d92bd0a30d52c
Vinh <55190465+kieuvinhh@users.noreply.github.com>
2021-10-12 21:07:21 +0700

    Addposter

--- src/Poster.js
+++ src/Poster.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line 
 import react from "react";
+import posters from "./poster.png";
 import "./Poster.css";
 function poster(){
     return(
         <div className="Body">
-            <img src={"../poster.png"} alt="" height="600" />
+            <img src={posters} alt="" height="600" />
         </div>
     );
 }
