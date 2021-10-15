5b8e65f7053aab265e51da1f1f9f1ea2a8af8b55
Vinh <55190465+kieuvinhh@users.noreply.github.com>
2021-10-15 14:38:56 +0700

    fix auto play video

--- src/components/comunity/Comunity.js
+++ src/components/comunity/Comunity.js
@@ -17,26 +17,26 @@ function Comunity() {
       <div className="Video">
         <div className="Video1">
           <iframe width="100%" height="300px" src="https://www.youtube.com/embed/oubyBG2QdgM"
-          title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+          title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; 
           encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
             </iframe>
           </div>
         <div className="video2">
           <iframe width="100%" height="300px" src="https://www.youtube.com/embed/Sp_vwyPcOXg"
-        title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+        title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; 
         encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
           </iframe>
         </div>
         <div className="Video3"> 
         <iframe width="100%" height="300px" src="https://www.youtube.com/embed/T2jtWGQsf9k" 
-        title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+        title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; 
         encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
           </iframe>
         </div>
 
         <div className="video4">
         <iframe width="100%" height="300px" src="https://www.youtube.com/embed/sDwvPCAWCL4"
-         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write;
+         title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write;
           encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
 
           </iframe>
