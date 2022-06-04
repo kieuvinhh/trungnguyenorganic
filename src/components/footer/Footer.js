05d112f22d14786dd5cbd8288a91c2306977e854
Vinh <55190465+kieuvinhh@users.noreply.github.com>
2022-06-04 15:45:01 +0700

    initial

--- src/components/footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import "./Footer.css"
-function Footer() {
-    return (
-        <div className="Footer">
-            <div id="Copyright">&copy; Copyright 2020 Trung Nguyen Organic</div>
-        </div>
-    );
-}
-export default Footer;
